1402846eb8350be767f0face2a0dfdb344c21044
Jalonzpa <joshuaal22@g.eips.ca>
2018-01-30 14:28:41 -0700

    New webserver file test

--- webeasy.py
+++ webeasy.py
@@ -4,7 +4,14 @@ import random
 from random import choice
 import subprocess
 import CHIP_IO.GPIO as GPIO
+from flask import Flask, request, render_template
+app = Flask(__name__)
 GPIO.setup("CSID0", GPIO.IN)
+button_name = ""
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 wavs = open('wavs.txt').read().splitlines()
 print(wavs)
@@ -19,4 +26,5 @@ def button():
       subprocess.call(['play', wav])
 
 if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0')
   button()
